eca7c3bcfedbd5476dd857121ddfe955c1b96b5a
bcorfman1 <bcorfman1@localhost>
2010-06-09 17:28:29 +0000

    Initial stab at writing a test case for OneKingAttackOneKingEvaluator

--- test_onek_eval.py
+++ test_onek_eval.py
@@ -1,28 +1,33 @@
+from globalconst import BLACK, WHITE, KING
 import onek_onek
 import unittest
+import checkers
+
+#   (white)
+#            45  46  47  48
+#          39  40  41  42
+#            34  35  36  37
+#          28  29  30  31
+#            23  24  25  26
+#          17  18  19  20
+#            12  13  14  15
+#          6   7   8   9
+#   (black)
 
 class TestOneKingAttackOneKingEvaluator(unittest.TestCase):
     def setUp(self):
         self.game = checkers.Checkers()
         self.board = self.game.curr_state
-        self.eval = onek_onek.OneKingAttackOneKingEvaluator()
+        self.board.clear()
+        squares = self.board.squares
+        squares[6] = BLACK | KING
+        squares[48] = WHITE | KING
+        self.board.to_move = BLACK
+        self.board.white_total = 1
+        self.board.black_total = 1
+
     def testInitialBlackMoves(self):
-        # Tests whether initial game moves are correct from
-        # Black's perspective
-        moves = [m for m, _ in self.game.successors(self.board)]
-        self.assertEqual(moves, [[[17, BLACK | MAN, FREE],
-                                  [23, FREE, BLACK | MAN]],
-                                 [[18, BLACK | MAN, FREE],
-                                  [23, FREE, BLACK | MAN]],
-                                 [[18, BLACK | MAN, FREE],
-                                  [24, FREE, BLACK | MAN]],
-                                 [[19, BLACK | MAN, FREE],
-                                  [24, FREE, BLACK | MAN]],
-                                 [[19, BLACK | MAN, FREE],
-                                  [25, FREE, BLACK | MAN]],
-                                 [[20, BLACK | MAN, FREE],
-                                  [25, FREE, BLACK | MAN]],
-                                 [[20, BLACK | MAN, FREE],
-                                  [26, FREE, BLACK | MAN]]])
-        
+        ev = onek_onek.OneKingAttackOneKingEvaluator(1.0)
+        self.assertEqual(ev.calculateDesirability(self.board), 1.0)
+
 
