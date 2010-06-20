c47c40be51a4d0d3297b3a9e9535f67c8b31f88d
Welcome <Welcome@Welcome-VAIO.myhome.westell.com>
2010-06-19 21:00:18 -0400

    First cut at creating Goal_Think, which arbitrates among all GoalEvaluators

---
+++ think.py
@@ -0,0 +1,37 @@
+from composite import CompositeGoal
+from onek_onek import OneKingAttackOneKingEvaluator, OneKingFleeOneKingEvaluator
+
+class Goal_Think(CompositeGoal):
+    def __init__(self):
+        self.evaluators = [OneKingAttackOneKingEvaluator(1.0),
+                           OneKingFleeOneKingEvaluator(1.0)]
+
+    def activate(self):
+        if self.owner.isPossessed():
+            self.arbitrate()
+        self.status = self.ACTIVE
+
+    def process(self):
+        self.activateIfInactive()
+        status = self.processSubgoals()
+        if status == self.COMPLETED or status == self.FAILED:
+            if not self.owner.isPossessed():
+                self.status = self.INACTIVE
+        return status
+
+    def terminate(self):
+        pass
+        
+    def arbitrate(self):
+        most_desirable = None
+        best_score = 0
+        for e in self.evaluators:
+            d = e.calculateDesirability()
+            if d > best_score:
+                most_desirable = e
+                best_score = d
+        if most_desirable == None:
+            raise ApplicationException('Goal_Think::arbitrate - no evaluator selected')
+        most_desirable.setGoal(self.owner)
+
+    
