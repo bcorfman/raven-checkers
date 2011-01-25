b9d553cc0d90524fa44ded45a4d547be8587c333
bcorfman <bcorfman@localhost>
2011-01-24 21:08:05 -0500

    Effbot's auto show/hide scrollbar for Tkinter

---
+++ autoscrollbar.py
@@ -0,0 +1,17 @@
+from Tkinter import *
+
+class AutoScrollbar(Scrollbar):
+    # a scrollbar that hides itself if it's not needed.  only
+    # works if you use the grid geometry manager.
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            # grid_remove is currently missing from Tkinter!
+            self.tk.call("grid", "remove", self)
+        else:
+            self.grid()
+        Scrollbar.set(self, lo, hi)
+    def pack(self, **kw):
+        raise TclError, "cannot use pack with this widget"
+    def place(self, **kw):
+        raise TclError, "cannot use place with this widget"
+    
