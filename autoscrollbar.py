216c7187f859d601656619c72828f856121de208
bcorfman <bcorfman@localhost>
2011-01-27 09:19:27 -0500

    Changes to scrollbar take into account a container, row & col and sticky attributes

--- autoscrollbar.py
+++ autoscrollbar.py
@@ -1,17 +1,27 @@
 from Tkinter import *
 
 class AutoScrollbar(Scrollbar):
+    def __init__(self, master=None, cnf={}, **kw):
+        self.container = kw.pop('container', None)
+        self.row = kw.pop('row', 0)
+        self.column = kw.pop('column', 0)
+        self.sticky = kw.pop('sticky', '')
+        Scrollbar.__init__(self, master, cnf, **kw)
+
     # a scrollbar that hides itself if it's not needed.  only
     # works if you use the grid geometry manager.
     def set(self, lo, hi):
         if float(lo) <= 0.0 and float(hi) >= 1.0:
             # grid_remove is currently missing from Tkinter!
-            self.tk.call("grid", "remove", self)
+            self.tk.call('grid', 'remove', self)
         else:
-            self.grid()
+            if not self.container:
+                self.grid()
+            else:
+                self.grid(in_=self.container, row=self.row,
+                          column=self.column, sticky=self.sticky)
         Scrollbar.set(self, lo, hi)
     def pack(self, **kw):
-        raise TclError, "cannot use pack with this widget"
+        raise TclError, 'cannot use pack with this widget'
     def place(self, **kw):
-        raise TclError, "cannot use place with this widget"
-    
+        raise TclError, 'cannot use place with this widget'
