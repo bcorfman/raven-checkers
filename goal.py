306d173f4a623e16838478de5f14ed224aede586
brandon_corfman <brandon_corfman@Cap.local>
2015-02-16 14:59:44 -0500

    Fixed to PEP8

--- goal.py
+++ goal.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Goal(object):
+class Goal:
     __metaclass__ = ABCMeta
 
     INACTIVE = 0
@@ -12,6 +12,18 @@ class Goal(object):
     def __init__(self, owner):
         self.owner = owner
         self.status = self.INACTIVE
+
+    @abstractmethod
+    def activate(self):
+        pass
+
+    @abstractmethod
+    def process(self):
+        pass
+
+    @abstractmethod
+    def terminate(self):
+        pass
 
     def handle_message(self, msg):
         return False
@@ -26,16 +38,3 @@ class Goal(object):
     def activate_if_inactive(self):
         if self.status == self.INACTIVE:
             self.status = self.ACTIVE
-
-    @abstractmethod
-    def activate(self):
-        pass
-
-    @abstractmethod
-    def process(self):
-        pass
-
-    @abstractmethod
-    def terminate(self):
-        pass
-
