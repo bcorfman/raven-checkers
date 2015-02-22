8ee551b0bcab8a204c4fda76f933dcb7165a8fb9
brandon_corfman <brandon_corfman@cap.local>
2015-02-21 23:34:32 -0500

    Matching formations now works with a best_first_graph_search.

--- shortdyke_eval.py
+++ shortdyke_eval.py
@@ -1,58 +1,69 @@
 __author__ = 'brandon_corfman'
 from goalevaluator import GoalEvaluator
-from csp import backtracking_search
-from formation import formation_csp, BLACK_MAP, WHITE_MAP, BLACK
-from globalconst import WHITE
+from formation import BLACK_MAP, WHITE_MAP, BLACK, MAN
+from globalconst import WHITE, BLACK_IDX, WHITE_IDX
 from shortdyke import GoalShortDyke
-from search import Problem, depth_first_tree_search
-from utils import manhattan_distance, argmin_random_tie
+from search import Problem, best_first_graph_search
+
+#   (white)
+#            45  46  47  48
+#          39  40  41  42
+#            34  35  36  37
+#          28  29  30  31
+#            23  24  25  26
+#          17  18  19  20
+#            12  13  14  15
+#          6   7   8   9
+#   (black)
 
 
 class FormationProblem(Problem):
-    def __init__(self, formation, board):
-        self.start_row = None
-        self.start_col = None
-        self.end_row = None
-        self.end_col = None
-        self.formation = formation
+    def __init__(self, goal, board):
         self.board = board
-        self.initial = []
+        self.initial = ()
+        self.goal = goal
 
-    def successor(self, state):
-        # assign next element in state
-        pos_map = BLACK_MAP if self.board.to_move == BLACK else WHITE_MAP
-        next_var = self.formation[len(state)]
-        goal_row, goal_col = self.board.gridmap[next_var]
+    def actions(self, state):
+        player = self.board.to_move
+        pos_map = BLACK_MAP if player == BLACK else WHITE_MAP
+        next_var = self.goal[len(state)]
+        actions = [pos for pos in pos_map[next_var] if self.board.squares[pos] == player + MAN]
+        for a in actions:
+            if a not in state:
+                yield a
 
-        def calc_dist(loc):
-            if loc not in state:
-                row, col = self.board.gridmap[loc]
-                return manhattan_distance(row, col, goal_row, goal_col)
-            else:
-                return 9999  # location has already been used, so don't let the distance be selected
-
-        best = argmin_random_tie(list(pos_map[next_var]), calc_dist)
-        return state + [best] if best else None
+    def result(self, state, action):
+        return state + (action, )
 
     def goal_test(self, state):
-        return len(state) == len(self.formation)
+        return len(state) == len(self.goal)
 
-    def value(self):
-        pass
+    def path_cost(self, c, state1, action, state2):
+        if not state2:
+            return 0
+        player = self.board.to_move
+        index_list = BLACK_IDX if player == BLACK else WHITE_IDX
+        start_sq = state2[-1]
+        goal_sq = self.goal[len(state2) - 1]
+        frontier = [(start_sq, c)]
+        while frontier:
+            curr_sq, cost = frontier.pop()
+            if curr_sq == goal_sq:
+                return cost
+            for i in index_list:
+                if curr_sq + i <= goal_sq:
+                    frontier.append((curr_sq + i, cost + 1))
+        return 99999
 
 
 def measure_formation_closeness(formation, board):
-    solution = depth_first_tree_search(FormationProblem(formation, board))
-    if solution:
-        diff = 0.0
-        for p in formation:
-            goal_row, goal_col = board.gridmap[p]
-            for s in solution:
-                soln_row, soln_col = board.gridmap[s]
-                diff += manhattan_distance(soln_row, soln_col, goal_row, goal_col)
-        return diff
-    else:
-        return 0.0
+    problem = FormationProblem(formation, board)
+
+    def f(current_node):
+        return sum(abs(s - formation[i]) for i, s in enumerate(current_node.state))
+
+    node = best_first_graph_search(problem, f)
+    return node.path_cost if node.solution() else 0.0
 
 
 class ShortDykeEvaluator(GoalEvaluator):
