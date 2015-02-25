93e41bd65f7da6f0e4320dc21e6f1304c2da8a92
brandon_corfman <brandon_corfman@cap.local>
2015-02-24 22:24:02 -0500

    Made desirability a yes or no evaluation

--- evaluators.py
+++ evaluators.py
@@ -24,7 +24,8 @@ class ShortDykeEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         short_dyke = [8, 9, 14, 17, 23, 29] if player == BLACK else [14, 17, 21, 27, 31, 32]
-        return measure_formation_closeness(short_dyke, board)
+        return 1.0 if measure_formation_closeness(short_dyke, board) else 0.0
+
 
     def set_goal(self, board):
         player = board.to_move
@@ -40,7 +41,7 @@ class LongDykeEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         long_dyke = [8, 9, 14, 19, 24, 29] if player == BLACK else [14, 18, 23, 27, 31, 32]
-        return measure_formation_closeness(long_dyke, board)
+        return 1.0 if measure_formation_closeness(long_dyke, board) else 0.0
 
     def set_goal(self, board):
         player = board.to_move
@@ -56,7 +57,7 @@ class PyramidEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         pyramid = [1, 2, 3, 6, 7, 10] if player == BLACK else [23, 26, 27, 30, 31, 32]
-        return measure_formation_closeness(pyramid, board)
+        return 1.0 if measure_formation_closeness(pyramid, board) else 0.0
 
     def set_goal(self, board):
         player = board.to_move
@@ -72,7 +73,7 @@ class PhalanxEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         phalanx = [5, 6, 7, 8, 9, 10, 11, 14, 15] if player == BLACK else [18, 19, 22, 23, 24, 25, 26, 27, 28]
-        return measure_formation_closeness(phalanx, board)
+        return 1.0 if measure_formation_closeness(phalanx, board) else 0.0
 
     def set_goal(self, board):
         player = board.to_move
@@ -88,7 +89,7 @@ class MillEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         mill = [1, 3, 5, 8, 9, 11, 14, 15, 18] if player == BLACK else [15, 18, 19, 22, 24, 25, 28, 30, 32]
-        return measure_formation_closeness(mill, board)
+        return 1.0 if measure_formation_closeness(mill, board) else 0.0
 
     def set_goal(self, board):
         player = board.to_move
@@ -104,7 +105,7 @@ class EchelonEvaluator(GoalEvaluator):
     def calculate_desirability(self, board):
         player = board.to_move
         echelon = [2, 3, 5, 6, 7, 9, 10, 14] if player == BLACK else [19, 23, 24, 26, 27, 28, 30, 31]
-        return measure_formation_closeness(echelon, board)
+        return 1.0 if measure_formation_closeness(echelon, board) else 0.0
 
     def set_goal(self, board):
         player = board.to_move
