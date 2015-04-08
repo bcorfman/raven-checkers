a9386a7905cbc2434a3f9dd07c209df73846c8e3
brandon_corfman <bcorfman@fastmail.fm>
2015-04-07 22:47:50 -0400

    Bug fixed tests slightly

--- testthink.py
+++ testthink.py
@@ -1,5 +1,5 @@
 __author__ = 'brandon_corfman'
-from globalconst import BLACK
+from globalconst import BLACK, MAN, FREE
 from Tkinter import Tk
 from mainframe import MainFrame
 import think
@@ -28,8 +28,13 @@ class TestThinkFromInitialBoard(unittest.TestCase):
         self.manager = mainframe.manager
         self.board = self.manager.model.curr_state
         self.board.to_move = BLACK
+        squares = self.board.squares
+        # make initial 11-15 move for Black
+        squares[18] = FREE
+        squares[24] = BLACK | MAN
 
     def testCorrectGoalSelected(self):
+        self.manager.controller1.end_turn()
         self.manager.controller2.start_turn()
         self.assertIn(repr(self.manager.controller2._thinker.subgoals[0]), ['GoalShortDyke', 'GoalLongDyke',
                                                                             'GoalEchelon', 'GoalPhalanx',
