a9f732fa4a7199a39f06fe5e9d77e1342fbd657a
Brandon Corfman <bcorfman@fastmail.fm>
2022-09-09 14:47:31 -0400

    FEATURE: new PDNReader class with tests

--- parsing/PDN.py
+++ parsing/PDN.py
@@ -1,72 +1,104 @@
 import os
+from typing import NamedTuple
 
 
 def _read_value(line):
     return line.split('"')[1]
 
 
+PDN = NamedTuple("PDN", [("event", str), ("site", str), ("date", str),
+                         ("round", str), ("white", str), ("black", str),
+                         ("result", str), ("fen", str)])
+
+
 class PDNReader:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.fileiter = None
-        self.event = ""
-        self.site = ""
-        self.date = ""
-        self.round = ""
-        self.white_player = ""
-        self.black_player = ""
-        self.result = ""
-        self.fen = ""
-        self.read_pdn_file(filepath)
+    def __init__(self, stream, source=""):
+        self._stream = stream
+        self._source = f"in {source}" if source else ""
+        self._lineno = 0
+        self.PDNs = []
+        self._reset_pdn_vars()
+        self._read_stream()
 
-    def read_pdn_file(self, filepath):
-        parse_pdn = {"[Event": self.read_event,
-                     "[Site": self.read_site,
-                     "[Date": self.read_date,
-                     "[Round": self.read_round,
-                     "[White": self.read_white_player,
-                     "[Black": self.read_black_player,
-                     "[Result": self.read_result,
-                     "1.": self.read_fen}
+    @classmethod
+    def from_string(cls, pdn_string):
+        from io import StringIO
+        stream = StringIO(pdn_string)
+        return cls(stream, "PDN string")
 
-        with open(filepath) as self.fileiter:
-            while True:
-                line = self.fileiter.readline()
-                if not line:
-                    raise IOError(f"Unexpected EOF in {os.path.basename(self.filepath)}")
-                for tag in parse_pdn:
-                    if line.stripleft().beginswith(tag):
-                        parse_pdn[tag](line)
+    @classmethod
+    def from_file(cls, filepath):
+        filename = os.path.basename(filepath)
+        with open(filepath) as stream:
+            return cls(stream, filename)
 
-    def read_event(self, line):
-        self.event = _read_value(line)
+    def _reset_pdn_vars(self):
+        self._event = None
+        self._site = None
+        self._date = None
+        self._round = None
+        self._black = None
+        self._white = None
+        self._result = None
+        self._fen = None
 
-    def read_site(self, line):
-        self.site = _read_value(line)
-
-    def read_date(self, line):
-        self.date = _read_value(line)
-
-    def read_round(self, line):
-        self.round = _read_value(line)
-
-    def read_white_player(self, line):
-        self.white_player = _read_value(line)
-
-    def read_black_player(self, line):
-        self.black_player = _read_value(line)
-
-    def read_result(self, line):
-        self.result = _read_value(line)
-
-    def read_fen(self, line):
-        if not self.result:
-            raise IOError("Didn't get a result value")
-        self.fen = line
+    def _read_stream(self):
+        parse_pdn = {"[Event": self._read_event,
+                     "[Site": self._read_site,
+                     "[Date": self._read_date,
+                     "[Round": self._read_round,
+                     "[White": self._read_white_player,
+                     "[Black": self._read_black_player,
+                     "[Result": self._read_result,
+                     "1.": self._read_fen}
+        self._lineno = 0
         while True:
-            line = self.fileiter.readline()
+            line = self._stream.readline()
             if not line:
-                raise IOError(f"Unexpected EOF in {os.path.basename(self.filepath)}")
-            self.fen += line
-            if self.result in line:
                 break
+            self._lineno += 1
+            line = line.rstrip('\n')
+            for tag in parse_pdn:
+                if line.lstrip().startswith(tag):
+                    parse_pdn[tag](line)
+                    break
+
+    def _read_event(self, line):
+        self._event = _read_value(line)
+
+    def _read_site(self, line):
+        self._site = _read_value(line)
+
+    def _read_date(self, line):
+        self._date = _read_value(line)
+
+    def _read_round(self, line):
+        self._round = _read_value(line)
+
+    def _read_white_player(self, line):
+        self._white = _read_value(line)
+
+    def _read_black_player(self, line):
+        self._black = _read_value(line)
+
+    def _read_result(self, line):
+        self._result = _read_value(line)
+
+    def _read_fen(self, line):
+        if not self._result:
+            raise IOError("Didn't get a result value")
+        self._fen = line.rstrip('\n')
+        if self._result not in line:
+            while True:
+                line = self._stream.readline()
+                if not line:
+                    error = f"Unexpected EOF {self._source} on " + \
+                            f"line {self._lineno}"
+                    raise IOError(error)
+                self._lineno += 1
+                self._fen += line.rstrip('\n')
+                if self._result in line:
+                    break
+        self.PDNs.append(PDN(self._event, self._site, self._date, self._round,
+                         self._white, self._black, self._result, self._fen))
+        self._reset_pdn_vars()
